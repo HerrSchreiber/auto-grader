da220ca37ff5a17658f60436715bf412e6127e11
HerrSchreiber <robert.schreiber@boiseschools.org>
2015-10-07 15:39:33 -0600

    Fix Finder Archive issue

--- AutoGrader.java
+++ AutoGrader.java
@@ -217,7 +217,7 @@ public class AutoGrader {
             if (list == null) return;
 
             for ( File f : list ) {
-                if ( f.isDirectory() && !f.getName().equals("_MACOSX")) {
+                if ( f.isDirectory() && !f.getName().equals("__MACOSX")) {
                     walkAndCompile( f.getAbsolutePath() );
                 }
                 else if (!f.isDirectory() && f.getName().substring(f.getName().length() - 5).equals(".java")) {
