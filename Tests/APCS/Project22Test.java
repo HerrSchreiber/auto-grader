f410223bdd49e143f3d782973331e1e2d19d5e00
Rob Schreiber <robert.schreiber@boiseschools.org>
2016-01-25 14:35:13 -0700

    Add Project 22 test

--- Tests/APCS/Project22Test.java
+++ Tests/APCS/Project22Test.java
@@ -2,7 +2,7 @@
  * This test outputs nothing so all that shows up in the program is just
  * the source code of their project.
  */
-public class Project14Test {
+public class Project22Test {
 	public static void main(String[] args) {
 		return;
 	}
