8f594e537dd7aacc70b4aaa1ad4d3a8e6320e4d8
herrschreiber <robert.schreiber@boiseschools.org>
2016-09-03 14:13:17 -0600

    Update to newer version and add WinRun4J executable

--- Tests/APCS/Project14Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * This test outputs nothing so all that shows up in the program is just
- * the source code of their project.
- */
-public class Project14Test {
-	public static void main(String[] args) {
-		return;
-	}
-}
--- Tests/APCS/Project22Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * This test outputs nothing so all that shows up in the program is just
- * the source code of their project.
- */
-public class Project22Test {
-	public static void main(String[] args) {
-		return;
-	}
-}
